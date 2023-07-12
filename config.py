32cb946537679fc5e68485dcc9a326b3d3d90821
Pavel <paleksandrov@vk.com>
2023-07-12 11:58:36 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6391839004:AAFuZcpfs5SrU5aGWoD9sfnI8Ho-pWxcakY'
+
+exchange = {'евро' : 'EUR',
+       'доллар' : 'USD',
+       'рубль' : 'RUB',
+       'биткоин': 'BTC',
+       'эфириум': 'ETH',
+       'юань': 'CNY',
+       }
